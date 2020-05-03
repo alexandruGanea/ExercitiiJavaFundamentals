34ea8d5e81ff002bd46978ab9040eda9e1d9811a
squirmezoid <64680013+squirmezoid@users.noreply.github.com>
2020-05-03 11:11:48 +0300

    optimizat codul cu FOR EACH in loc de FOR

--- Exercitiul3/src/Main.java
+++ Exercitiul3/src/Main.java
@@ -8,21 +8,19 @@ public class Main {
                 "for statement, so it can be used in the termination and increment expressions as well.";
 
         int counter = wordCount(text1);
-        System.out.println("Ex3. Numarul de cuvinte cu lungime impara si care se termina cu \"e\" este: " + counter);
+        System.out.println(". Numarul de cuvinte cu lungime impara si care se termina cu \"e\" este: " + counter);
 
 
     }
 
-    public static int wordCount(String text1){
+    public static int wordCount(String text1) {
         String[] stringArray = text1.split(" ");
-        System.out.print("Cuvintele cerute sunt: ");
+        System.out.print("Ex3. Cuvintele cerute sunt: ");
         int count = 0;
-        for(int c=0;  c<stringArray.length; c++){
-            if (stringArray[c].length()%2!=0){
-                if(stringArray[c].endsWith("e")){
-                    System.out.print(stringArray[c] + ", ");
-                    count++;
-                }
+        for (String s : stringArray) {
+            if (s.length() % 2 != 0 && s.endsWith("e")) {
+                System.out.print(s + ", ");
+                count++;
             }
         }
         return count;
