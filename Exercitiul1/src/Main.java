2e16b34bc2333b4ee0df1d298c44b20772b3fa9b
squirmezoid <64680013+squirmezoid@users.noreply.github.com>
2020-05-03 10:18:35 +0300

    rezolvat cerinta 1

---
+++ Exercitiul1/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Scrieti o functie care primeste 2 String-uri ca parametru si verifica daca primul String il contine pe al doilea.
+
+        String string1 = "Ana are pere de aceasta data. Ionel vrea si el, dar nu primeste.";
+        String string2 = "are pere";
+        System.out.print("Ex1. ");
+
+        stringContains(string1, string2);
+    }
+
+    public static void stringContains(String string1, String string2) {
+        if (string1.contains(string2)) {
+            System.out.println("\"" + string2 + "\"" + " este inclus in " + "\"" + string1 + "\"");
+        } else {
+            System.out.println("\"" + string2 + "\"" + " nu este inclus in " + "\"" + string1 + "\"");
+        }
+    }
+}
