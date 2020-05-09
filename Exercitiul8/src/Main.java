2aae879fe9aa61cc502ed47fa3a63a2e0bd5d7d6
squirmezoid <64680013+squirmezoid@users.noreply.github.com>
2020-05-09 11:51:36 +0300

    adaugat cod ptr operatii cu Scanner

--- Exercitiul8/src/Main.java
+++ Exercitiul8/src/Main.java
@@ -57,28 +57,76 @@ public class Main {
                 n1 = s2.nextInt();
                 System.out.print("Introduceti al doilea numar: ");
                 n2 = s2.nextInt();
-                System.out.println("Rezultatul ridicarii la putere al numerelor " + n1 + " si " + n2 +" este: " + calculatorStiintific.exponentiation(n1, n2));
+                System.out.println("Rezultatul ridicarii la putere al numerelor " + n1 + " si " + n2 + " este: " + calculatorStiintific.exponentiation(n1, n2));
                 break;
             case 2:
                 System.out.print("Introduceti un numar: ");
                 n1 = s2.nextInt();
                 System.out.println("Rezultatul radicalului din " + n1 + " este: " + calculatorStiintific.sqrt(n1));
                 break;
-                case 3:
-                    System.out.print("Introduceti un numar: ");
-                    n1 = s2.nextInt();
-                    System.out.println("Rezultatul logaritmului in baza 10 a numarului " + n1 + " este: " + calculatorStiintific.log10(n1));
-                    break;
+            case 3:
+                System.out.print("Introduceti un numar: ");
+                n1 = s2.nextInt();
+                System.out.println("Rezultatul logaritmului in baza 10 a numarului " + n1 + " este: " + calculatorStiintific.log10(n1));
+                break;
             case 4:
                 System.out.print("Introduceti un numar: ");
                 n1 = s2.nextInt();
-                System.out.println("Rezultatul logaritmului natural al numarului " +n1 + " este: " + calculatorStiintific.log(n1));
+                System.out.println("Rezultatul logaritmului natural al numcalculator stiintificarului " + n1 + " este: " + calculatorStiintific.log(n1));
                 break;
             default:
                 System.out.println("Numarul introdus nu corespunde unei operatii permise");
         }
 
+
+        // Sa se introduca 2 numere
+        //Afisam numerele
+        // Sa se afiseze meniul
+        // 1. Adunare
+        // 2. Inmultire
+        // 3. Radical
+        // 4. Scadere
+        // Afisam mesajul "Va rugam alegeti o optiune"
+        //In functie de optiunea aleasa se realizeaza operatia respectiva, daca operatia presupune folosirea unui singur numar
+        // facem operatia si ptr al doilea nr introdus
+        // Afisam rezultatul
+
+
+        Scanner s3 = new Scanner(System.in);
+        System.out.println("Va rugam introduceti cele 2 numere");
+        n1 = s3.nextInt();
+        n2 = s3.nextInt();
+        System.out.println("Numerele introduse sunt: " + n1 + " si " + n2);
+        System.out.println("Operatii (alegeti numarul corespunzator, sau \"0\" ptr inapoi: \n  --1-- ADUNARE\n  --2-- INMULTIRE\n  --3-- RADICAL\n  --4-- SCADERE");
+        int option2 = s3.nextInt();
+        while (option2 != 0) {
+            switch (option2) {
+                case 1:
+                    System.out.println("ADUNARE: Rezultatul este: " + calculatorStiintific.sum(n1, n2));
+                    System.out.println("Introduceti  alt numar 1...4 pentru alta operatie, sau 0 ptr STOP");
+                    break;
+                case 2:
+                    System.out.println("INMULTIRE: Rezultatul este " + calculatorStiintific.mult(n1, n2));
+                    System.out.println("Introduceti  alt numar 1...4 pentru alta operatie, sau 0 ptr STOP");
+                    break;
+                case 3:
+                    System.out.println("RADICAL: Rezultatul primului nr introdus este " + calculatorStiintific.sqrt((n1)));
+                    System.out.println("RADICAL: Rezultatul celui de-al doilea nr introdus este " + calculatorStiintific.sqrt(n2));
+                    System.out.println("Introduceti  alt numar 1...4 pentru alta operatie, sau 0 ptr STOP");
+                    break;
+                case 4:
+                    System.out.println("SCADERE: Rezultatul este: " + calculatorStiintific.diff(n1, n2));
+                    System.out.println("Introduceti  alt numar 1...4 pentru alta operatie, sau 0 ptr STOP");
+                    break;
+                default:
+                    System.out.println("Optiunea aleasa nu exista, vezi meniul.");
+                    System.out.println("Introduceti  alt numar 1...4 pentru alta operatie, sau 0 ptr STOP");
+            }
+            option2 = s3.nextInt();
+        }
+        System.out.println("Ati apasat 0. O zi buna!");
     }
 
 }
+
 
