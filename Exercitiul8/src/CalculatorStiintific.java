87e0e347fdfdf6d97d4d739772cfc65add81d3eb
squirmezoid <64680013+squirmezoid@users.noreply.github.com>
2020-05-09 11:04:43 +0300

    adaugat calculator stiintific

---
+++ Exercitiul8/src/CalculatorStiintific.java
@@ -0,0 +1,22 @@
+public class CalculatorStiintific  extends Calculator{
+
+    public CalculatorStiintific(){
+
+    }
+
+    public double exponentiation(int p1, int p2){
+        return Math.pow(p1, p2);
+    }
+
+    public double sqrt(int p1){
+        return Math.sqrt(p1);
+    }
+
+    public double log10(int p1){
+        return Math.log10(p1);
+    }
+
+    public double log(int p1){
+        return Math.log(p1);
+    }
+}
--- Exercitiul8/src/Main.java
+++ Exercitiul8/src/Main.java
@@ -15,11 +15,11 @@ public class Main {
 
 
         //de aici citim valorile din tastatura - obiectul care ne ajuta se numeste Scanner
-        Scanner scanner = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
         System.out.print("Va rugam introduceti primul numar: ");
-        int n1 = scanner.nextInt();
+        int n1 = s1.nextInt();
         System.out.print("Va rugam introduceti al doilea numar: ");
-        int n2 = scanner.nextInt();
+        int n2 = s1.nextInt();
         System.out.println("Numerele introduse de Dvs sunt: " + n1 + ", " + n2);
 
         // Afisam rezultatele operatiilor
@@ -34,6 +34,49 @@ public class Main {
 
         int div = calculator.div(n1, n2);
         System.out.println("Rezultatul inmultirii este: " + div);
+
+        // sa se modeleze un caculator stiintific care sa permita operatiilr calculatorului simplu, si in plus":
+        // -- un numar ridicat la puterea altui numar
+        // -- radical dintr-un numar
+        // -- logaritm in baza 10 dintr-un numar
+        // -- logaritm dintr-un numar
+
+        CalculatorStiintific calculatorStiintific = new CalculatorStiintific();
+        System.out.println("Rezultatul ridicarii la putere este: " + calculatorStiintific.exponentiation(2, 8));
+        System.out.println("Rezultatul radicalului este: " + calculatorStiintific.sqrt(25));
+        System.out.println("Rezultatul log10 este: " + calculatorStiintific.log10(25));
+        System.out.println("Rezultatul log este: " + calculatorStiintific.log(55));
+
+
+        Scanner s2 = new Scanner(System.in);
+        System.out.println("Introduceti cifra operatiei: \n   1 - Ridicare la putere\n   2 - Radical\n   3 - Logaritm in baza 10\n   4 - Logaritm natural");
+        int option = s2.nextInt();
+        switch (option) {
+            case 1:
+                System.out.print("Introduceti primul numar: ");
+                n1 = s2.nextInt();
+                System.out.print("Introduceti al doilea numar: ");
+                n2 = s2.nextInt();
+                System.out.println("Rezultatul ridicarii la putere al numerelor " + n1 + " si " + n2 +" este: " + calculatorStiintific.exponentiation(n1, n2));
+                break;
+            case 2:
+                System.out.print("Introduceti un numar: ");
+                n1 = s2.nextInt();
+                System.out.println("Rezultatul radicalului din " + n1 + " este: " + calculatorStiintific.sqrt(n1));
+                break;
+                case 3:
+                    System.out.print("Introduceti un numar: ");
+                    n1 = s2.nextInt();
+                    System.out.println("Rezultatul logaritmului in baza 10 a numarului " + n1 + " este: " + calculatorStiintific.log10(n1));
+                    break;
+            case 4:
+                System.out.print("Introduceti un numar: ");
+                n1 = s2.nextInt();
+                System.out.println("Rezultatul logaritmului natural al numarului " +n1 + " este: " + calculatorStiintific.log(n1));
+                break;
+            default:
+                System.out.println("Numarul introdus nu corespunde unei operatii permise");
+        }
 
     }
 
